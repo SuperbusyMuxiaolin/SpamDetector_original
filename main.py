2b897e4eb55d7a64a90ed3700baccb7c85e660b1
SuperbusyMuxiaolin <2823466280@qq.com>
2024-06-02 16:55:48 +0800

    完善代码

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from spamdetect import Spamdetect
 
 spamdetect = Spamdetect()
 X, y = spamdetect.getfeature("processed_train_data")
-#spamdetect.train(X, y)
-x2, y2 = spamdetect.getfeature("processed_test_data")
-spamdetect.predict(x2, y2)
+spamdetect.train(X, y)
+X2, y2 = spamdetect.getfeature("processed_test_data")
+spamdetect.predict(X2, y2)
--- spamdetect.py
+++ spamdetect.py
@@ -8,10 +8,28 @@ import json
 class Spamdetect:
 
     def __init__(self):
-        self.word_counts = None
+        self.word_counts = {'spam': {}, 'ham': {}}
         self.log_class_p = None
         self.num_emails = None
-        self.all_words = None
+        self.all_words = []
+        # 停用词，对分类几乎没影响
+        self.stop_words = {"a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are",
+                           "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both",
+                           "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does",
+                           "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further",
+                           "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's",
+                           "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i",
+                           "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its",
+                           "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of",
+                           "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out",
+                           "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't",
+                           "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them",
+                           "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're",
+                           "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was",
+                           "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's",
+                           "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why",
+                           "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've",
+                           "your", "yours", "yourself", "yourselves", "subject"}
 
     # 将文件名和标签分开
     def getfeature(self, directory=None):
@@ -21,11 +39,8 @@ class Spamdetect:
         for file in train_files:
             label = int(file.split('_')[0])  # 获取文件名中的标签（第一个值）
             with open(os.path.join(directory, file), encoding='latin-1') as f:
-                X.append(f.read())
-                y.append(label)
-        # print(len(X))
-        # print(len(y))
-        # print(y.__getitem__(0))
+                X.append(f.read())  # 存储邮件内容的字符串列表
+                y.append(label)  # 存储标签的int列表
         return X, y
 
     # 去除所有标点符号
@@ -33,12 +48,13 @@ class Spamdetect:
         translator = str.maketrans("", "", string.punctuation)
         return s.translate(translator)
 
-    # 分开每个单词
+    # 分开每个单词并全部设定为小写，去除停用词
     def tokenize(self, text):
         text = self.clean(text).lower()
-        return re.split("\W+", text)
+        words = re.split("\W+", text)
+        return [word for word in words if word not in self.stop_words and word != ""]
 
-    # 计算某个单词出现的次数
+    # 计算words列表中每个单词出现的频率
     def get_word_counts(self, words):
         word_counts = {}
         for word in words:
@@ -49,21 +65,14 @@ class Spamdetect:
         # 存储邮件两种种类的个数
         self.num_emails = {"spam": sum(1 for label in labels if label == 1),
                            "ham": sum(1 for label in labels if label == 0)}
-        with open('num_emails.json', 'w', encoding='utf-8') as f:
-            json.dump(self.num_emails, f, ensure_ascii=False, indent=4)
-        # 两种邮件比例的log
+
+        # 存储两种邮件比例的log
         self.log_class_p = {
             "spam": math.log(self.num_emails['spam'] / (self.num_emails['spam'] + self.num_emails['ham'])),
             "ham": math.log(self.num_emails['ham'] / (self.num_emails['spam'] + self.num_emails['ham']))
         }
-        # 将log_class_p字典保存为 JSON 文件, 方便调用模型
-        with open('log_class_p.json', 'w', encoding='utf-8') as f:
-            json.dump(self.log_class_p, f, ensure_ascii=False, indent=4)
 
-        self.all_words = []
-
-        self.word_counts = {'spam': {}, 'ham': {}}
-
+        # 统计两种邮件各自单词词频以及所有出现的单词
         k = 0
         for f, la in zip(features, labels):
             k += 1
@@ -73,21 +82,22 @@ class Spamdetect:
             else:
                 c = 'ham'
             single_words_counts = self.get_word_counts(self.tokenize(f))
-            print(single_words_counts)
+            # print(single_words_counts)
             for word, count in single_words_counts.items():
+                # 如果单词不再all_words中，则将单词加入all_words中
                 if word not in self.all_words:
                     self.all_words.append(word)
-                # print(word)
-                # print(all_words)
                 # 如果word不在字典里，先初始化以word为第二层键值的count值为0
                 if word not in self.word_counts[c]:
                     self.word_counts[c][word] = 0.0
-                self.word_counts[c][word] += 1
-                # self.word_counts[c][word] += count
+                # self.word_counts[c][word] += 1
+                self.word_counts[c][word] += count
 
-        # print(self.word_counts.__getitem__('spam'))
-        # print(self.log_class_p["spam"])
-        # print(self.log_class_p["ham"])
+        # 将参数保存为json文件, 方便调用模型
+        with open('num_emails.json', 'w', encoding='utf-8') as f:
+            json.dump(self.num_emails, f, ensure_ascii=False, indent=4)
+        with open('log_class_p.json', 'w', encoding='utf-8') as f:
+            json.dump(self.log_class_p, f, ensure_ascii=False, indent=4)
         with open('all_words.json', 'w', encoding='utf-8') as f:
             json.dump(self.all_words, f, ensure_ascii=False, indent=4)
         with open('word_counts.json', 'w', encoding='utf-8') as f:
@@ -97,13 +107,13 @@ class Spamdetect:
         # 读取 JSON 文件中的模型数据
         with open('num_emails.json', 'r', encoding='utf-8') as f:
             num_emails = json.load(f)
+            print("spam:", num_emails["spam"])
+            print("ham:", num_emails["ham"])
 
         with open('log_class_p.json', 'r', encoding='utf-8') as f:
             log_class_p = json.load(f)
-        # print(log_class_p)
         with open('word_counts.json', 'r', encoding='utf-8') as f:
             word_counts = json.load(f)
-        # print(word_counts)
         with open('all_words.json', 'r', encoding='utf-8') as f:
             all_words = json.load(f)
         y_predict = []
@@ -116,40 +126,18 @@ class Spamdetect:
             for word, count in single_word_counts.items():
                 if word not in all_words:
                     continue
-                else:
-                    if word in word_counts['spam'].keys() and word in word_counts['ham'].keys():
-                        # 词集模型,每个词在同一篇邮件里只考虑其是否出现，不考虑同一篇邮件里的词频
-                        # log_word_given_spam = math.log(
-                        #     (word_counts['spam'][word] + 1) / (num_emails['spam'] + len(all_words)))
-                        # log_word_given_ham = math.log(
-                        #     (word_counts['ham'][word] + 1) / (num_emails['ham'] + len(all_words)))
-                        # 词袋模型，每个词还考虑了词在一篇邮件里出现的频率
-                        log_word_given_spam = math.log(
-                            (word_counts['spam'][word] + 1) / (sum(word_counts['spam'].values()) + len(all_words)))
-                        log_word_given_ham = math.log(
-                            (word_counts['ham'][word] + 1) / (sum(word_counts['ham'].values()) + len(all_words)))
-                    if word in word_counts['spam'].keys() and word not in word_counts['ham'].keys():
-                        # 词集模型,每个词在同一篇邮件里只考虑其是否出现，不考虑同一篇邮件里的词频
-                        # log_word_given_spam = math.log(
-                        #     (word_counts['spam'][word] + 1) / (num_emails['spam'] + len(all_words)))
-                        # log_word_given_ham = math.log(
-                        #     1 / (num_emails['ham'] + len(all_words)))
-                        # 词袋模型，每个词还考虑了词在一篇邮件里出现的频率
-                        log_word_given_spam = math.log(
-                           (word_counts['spam'][word] + 1) / (sum(word_counts['spam'].values()) + len(all_words)))
-                        log_word_given_ham = math.log(
-                           1 / (sum(word_counts['ham'].values()) + len(all_words)))
-                    if word not in word_counts['spam'].keys() and word not in word_counts['ham'].keys():
-                        # 词集模型,每个词在同一篇邮件里只考虑其是否出现，不考虑同一篇邮件里的词频
-                        log_word_given_spam = math.log(
-                            1 / (num_emails['spam'] + len(all_words)))
-                        log_word_given_ham = math.log(
-                            (word_counts['ham'][word] + 1) / (num_emails['ham'] + len(all_words)))
-                        # 词袋模型，每个词还考虑了词在一篇邮件里出现的频率
-                        log_word_given_spam = math.log(
-                           1 / (sum(word_counts['spam'].values()) + len(all_words)))
-                        log_word_given_ham = math.log(
-                           (word_counts['ham'][word] + 1) / (sum(word_counts['ham'].values()) + len(all_words)))
+                word_in_spam = word_counts['spam'].get(word, 0.0)
+                word_in_ham = word_counts['ham'].get(word, 0.0)
+                # 还考虑了单词在单篇文章中出现的次数
+                log_word_given_spam = math.log(
+                    (word_in_spam + 1) / (sum(word_counts['spam'].values()) + len(all_words)))
+                log_word_given_ham = math.log((word_in_ham + 1) / (sum(word_counts['ham'].values()) + len(all_words)))
+
+                # 仅仅考虑单词在单篇文章中是否出现
+                # log_word_given_spam = math.log(
+                #     (word_in_spam + 1) / (num_emails['spam'] + len(all_words)))
+                # log_word_given_ham = math.log((word_in_ham + 1) / (num_emails['spam'] + len(all_words)))
+                count = single_word_counts.get(word, 0.0)
                 spam += log_word_given_spam
                 ham += log_word_given_ham
             if spam > ham:
@@ -157,10 +145,10 @@ class Spamdetect:
             else:
                 y_predict.append(0)
         wright = 0
-        all_test=0
-        for y_p,l_t in zip(y_predict,labels_t):
-            all_test +=1
-            if y_p==l_t:
-                wright+=1
-        accuracy=wright/all_test
+        all_test = 0
+        for y_p, l_t in zip(y_predict, labels_t):
+            all_test += 1
+            if y_p == l_t:
+                wright += 1
+        accuracy = wright / all_test
         print(accuracy)
